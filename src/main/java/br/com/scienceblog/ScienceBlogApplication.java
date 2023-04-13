5ed3201f60f647b9aefaf8c4e199b743a8b8890e
RamonJales <ramonjales123@gmail.com>
2023-04-13 12:52:21 -0300

    First Commit

---
+++ src/main/java/br/com/scienceblog/ScienceBlogApplication.java
@@ -0,0 +1,13 @@
+package br.com.scienceblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScienceBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/scienceblog/ScienceBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scienceblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
