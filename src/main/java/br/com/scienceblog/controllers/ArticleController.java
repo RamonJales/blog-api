54a5ecf62f232b15cea001b120af5af850b7a5d6
RamonJales <ramonjales123@gmail.com>
2023-05-16 16:29:17 -0300

    DescriptionText column added

--- src/main/java/br/com/scienceblog/controllers/ArticleController.java
+++ src/main/java/br/com/scienceblog/controllers/ArticleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/article")
+@RequestMapping("/article/v1")
 public class ArticleController {
     final ArticleService articleService;
 
