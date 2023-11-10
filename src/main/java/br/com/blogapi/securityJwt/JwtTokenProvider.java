e0606305e9af40ea8f7e9c2fa3049734de7d2cf4
RamonJales <ramonjales123@gmail.com>
2023-11-10 15:26:43 -0300

    JwtTokenProvider implemented

---
+++ src/main/java/br/com/blogapi/securityJwt/JwtTokenProvider.java
@@ -0,0 +1,123 @@
+package br.com.blogapi.securityJwt;
+
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import br.com.blogapi.controllers.exceptions.InvalidJwtAuthenticationException;
+import br.com.blogapi.data.vo.v1.security.TokenVO;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+
+
+//class in which creates the token
+@Service
+public class JwtTokenProvider {
+	
+	@Value("${security.jwt.token.secret-key:secret}")
+	private String secretKey = "secret";
+
+	@Value("${security.jwt.token.expire-length:3600000}")
+	private long validtyInMilliseconds = 3600000; //1 hour
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	Algorithm algorithm = null;
+	
+	@PostConstruct
+	protected void init() {
+		secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+		algorithm = Algorithm.HMAC256(secretKey.getBytes());
+	}
+		
+	public TokenVO creatAccessToken(String username, List<String> roles) {
+		Date now = new Date();
+		Date validity = new Date(now.getTime()+validtyInMilliseconds);
+		String accessToken = getAccessToken(username, roles, now, validity);
+		String refreshToken = getRefreshToken(username, roles, now);
+		return new TokenVO(username, true, now, validity, accessToken, refreshToken);
+	}
+
+	private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+		String issuerUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toString();
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validity)
+				.withSubject(username)
+				.withIssuer(issuerUrl)
+				.sign(algorithm)
+				.strip();
+	}
+
+	private String getRefreshToken(String username, List<String> roles, Date now) {
+		Date validityRefreshToken = new Date(now.getTime()+(validtyInMilliseconds*3));
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validityRefreshToken)
+				.withSubject(username)
+				.sign(algorithm)
+				.strip();
+	}
+	
+	public Authentication getAuthentication(String token) {
+		DecodedJWT decoded = decodedToken(token);
+		UserDetails userDetails = this.userDetailsService.loadUserByUsername(decoded.getSubject());
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+	}
+
+	private DecodedJWT decodedToken(String token) {
+		Algorithm alg = Algorithm.HMAC256(secretKey.getBytes());
+		JWTVerifier verifier = JWT.require(alg).build();
+		DecodedJWT decodedJWT = verifier.verify(token);
+		return decodedJWT;
+	}
+	
+	public String resolveToken(HttpServletRequest req) {
+		String bearerToken = req.getHeader("Authorization");
+		if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring("Bearer ".length());
+		}
+		return null;
+	}
+	
+	public Boolean validateToken(String token) {
+		DecodedJWT decodedJWT = decodedToken(token);
+		try {
+			if(decodedJWT.getExpiresAt().before(new Date())) {
+				return false;
+			}
+			return true;
+		} catch (Exception e) {
+			throw new InvalidJwtAuthenticationException("Expired or invalid JWT token!");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
