b86dc4f46460a855e7fedd44c37d6277f1cc1946
RamonJales <ramonjales123@gmail.com>
2023-11-10 14:21:42 -0300

    InvalidJwtAuthentication, AccontCredentialsVO and TokenVo created

--- src/main/java/br/com/blogapi/controllers/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/blogapi/controllers/exceptions/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import br.com.blogapi.controllers.exceptions.InvalidJwtAuthenticationException;
 import br.com.blogapi.controllers.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -27,6 +28,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(),
                 HttpStatus.NOT_FOUND,
                 ex.getMessage());
+        response.getErrors().add(ex.getMessage());
+
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+    
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(InvalidJwtAuthenticationException ex) {
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(),
+                HttpStatus.FORBIDDEN,
+                ex.getMessage());
+        
         response.getErrors().add(ex.getMessage());
 
         return new ResponseEntity<>(response, response.getStatus());
